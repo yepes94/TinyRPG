28409c59198656daa1ad7ba25ec2314a777bc7c8
marcmateo <marc.mp44@gmail.com>
2018-12-03 12:04:53 +0100

    commit malo

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,31 +4,35 @@ using System.Collections;
 
 namespace Completed
 {
-	//Enemy inherits from MovingObject, our base class for objects that can move, Player also inherits from this.
+    //Enemy inherits from MovingObject, our base class for objects that can move, Player also inherits from this.
     public class Enemy : MonoBehaviour
-	{
+    {
         public int speed = 4;
         public int direccionAnterior;
-        public int playerDamage; 							//The amount of health points to subtract from the player when attacking.
-		//public AudioClip attackSound1;						//First of two audio clips to play when attacking the player.
-		//public AudioClip attackSound2;						//Second of two audio clips to play when attacking the player.
+        public int playerDamage;                            //The amount of health points to subtract from the player when attacking.
+                                                            //public AudioClip attackSound1;						//First of two audio clips to play when attacking the player.
+                                                            //public AudioClip attackSound2;						//Second of two audio clips to play when attacking the player.
         private Rigidbody2D enemyRB;
-		
-		private Animator animator;							//Variable of type Animator to store a reference to the enemy's Animator component.
-		private Transform target;                           //Transform to attempt to move toward each turn.
+
+        private Animator animator;                          //Variable of type Animator to store a reference to the enemy's Animator component.
+        private Transform target;                           //Transform to attempt to move toward each turn.
                                                             //private bool skipMove;								//Boolean to determine whether or not enemy should skip a turn or move this turn.
         private Vector2 move;
-        private bool moveUp = true;
+        public bool moveUp = true;
+        public bool moveRight = true;
+        public bool moveHorizontal = true;
+        public bool moveVertical = false;
 
 
-        private void Update(){
+        private void Update()
+        {
             MoveEnemy();
 
         }
 
-		//Start overrides the virtual Start function of the base class.
-		protected void Start ()
-		{
+        //Start overrides the virtual Start function of the base class.
+        protected void Start()
+        {
             //Register this enemy with our instance of GameManager by adding it to a list of Enemy objects. 
             //This allows the GameManager to issue movement commands.
             //GameManager.instance.AddEnemyToList (this);
@@ -36,31 +40,79 @@ namespace Completed
             enemyRB = GetComponent<Rigidbody2D>();
 
             //Get and store a reference to the attached Animator component.
-            animator = GetComponent<Animator> ();
-			
-			//Find the Player GameObject using it's tag and store a reference to its transform component.
-			target = GameObject.FindGameObjectWithTag ("Player").transform;
-			
-			//Call the start function of our base class MovingObject.
-			//base.Start ();
-		}
-		
+            animator = GetComponent<Animator>();
 
-		//MoveEnemy is called by the GameManger each turn to tell each Enemy to try to move towards the player.
-		public void MoveEnemy ()
-		{
-            if (moveUp)
-                move = new Vector2 (0, 1);
-            else
-                move = new Vector2 (0, -1);
-             
+            //Find the Player GameObject using it's tag and store a reference to its transform component.
+            target = GameObject.FindGameObjectWithTag("Player").transform;
 
-		}
+            //Call the start function of our base class MovingObject.
+            //base.Start ();
+        }
+
+
+        //MoveEnemy is called by the GameManger each turn to tell each Enemy to try to move towards the player.
+        public void MoveEnemy()
+        {
+            if (!moveVertical && !moveHorizontal)
+            {
+                bool TF = false;
+                TF = Random.Range(0, 1) == 0 ? false : true;
+
+                if (TF)
+                    moveVertical = true;
+                else
+                    moveHorizontal = true;
+
+            }
+
+            if (moveVertical)
+            {
+
+                moveHorizontal = false;
+
+                if (moveUp)
+                {
+                    move = new Vector2(0, 1);// coordenades (x,y) para arriba
+                    animator.SetFloat("movX", move.x);
+                    animator.SetFloat("movY", move.y);
+                }
+                else
+                {
+                    move = new Vector2(0, -1); //xy para abajo
+                    animator.SetFloat("movX", move.x);
+                    animator.SetFloat("movY", move.y);
+
+                }
+            }
+
+            if (moveHorizontal)
+            {
+
+                moveVertical = false;
+
+                if (moveRight)
+                {
+                    move = new Vector2(1, 0);// coordenades (x,y) para derecha
+                    animator.SetFloat("movX", move.x);
+                    animator.SetFloat("movY", move.y);
+
+                }
+
+                else
+                {
+                    move = new Vector2(-1, 0); //xy para izquierda
+                    animator.SetFloat("movX", move.x);
+                    animator.SetFloat("movY", move.y);
+                }
+            }
+
+
+        }
 
         private void FixedUpdate()
         {
-                enemyRB.MovePosition(enemyRB.position + move * speed * Time.deltaTime);
-                //enemyRB.MovePosition(enemyRB.position + direccionAnterior * speed * Time.deltaTime);
+            enemyRB.MovePosition(enemyRB.position + move * speed * Time.deltaTime);
+            //enemyRB.MovePosition(enemyRB.position + direccionAnterior * speed * Time.deltaTime);
 
         }
 
@@ -71,6 +123,11 @@ namespace Completed
                 moveUp = false;
             else
                 moveUp = true;
+
+            if (moveRight)
+                moveRight = false;
+            else
+                moveRight = true;
         }
 
     }
